ced18aa2c5d4b3fed6c6bd1417aec3c291132a1c
Loganathan <loganathan@theproindia.com>
2023-02-14 17:27:35 +0530

    initial commit

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import Table from "../table/table";
+
+const HomePage = () => {
+  const rowConfigURL = "http://localhost:4000/rowConfig";
+  const columnsConfigURL = "http://localhost:4000/columnsConfig";
+
+  let [columnsConfig, setcolumnsConfig] = useState<any>([]);
+  let [rowConfig, setrowConfig] = useState<any>({});
+  let [table, setTable] = useState(<></>);
+  let [checkboxData, setCheckBox] = useState([]);
+
+  // column data
+  useEffect(() => {
+    async function fetchcolumnData() {
+      let res = await fetch(columnsConfigURL);
+      let resp = await res.json();
+      setcolumnsConfig(resp);
+    }
+    fetchcolumnData();
+  }, []);
+
+  //Row data
+  useEffect(() => {
+    async function fetchRowData() {
+      let res = await fetch(rowConfigURL);
+      let resp = await res.json();
+      setrowConfig(resp);
+    }
+    fetchRowData();
+  }, []);
+
+  const HandleSubmitButton = () => {
+    let rowFields: any = [];
+    checkboxData.forEach((k: any) => {
+      Object.keys(rowConfig).forEach((key: any) => {
+        if (key === k["field"]) {
+          // @ts-ignore
+          rowFields.push({ [key]: rowConfig[key] });
+        }
+      });
+    });
+    console.log(rowFields);
+    setTable(<Table columnData={checkboxData} rowData={rowConfig} />);
+  };
+
+  return (
+    <>
+      <h2>Please select... </h2>
+      <ul>
+        {columnsConfig.map((item: any, index: number) => (
+          <li key={item.field}>
+            <input
+              type="checkbox"
+              name={item.field}
+              value={index}
+              onChange={(e: any) => {
+                let checked = e.target.checked;
+                if (checked) {
+                  // @ts-ignore
+                  setCheckBox([...checkboxData, item]);
+                }
+              }}
+            />
+            <label>{item.field}</label>
+          </li>
+        ))}
+      </ul>
+      <div className="bg-cyan-500"></div>
+      <div>
+        <button onClick={() => HandleSubmitButton()}>Submit</button>
+        {table}
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/components/table/table.tsx
@@ -0,0 +1,122 @@
+import React, { useMemo, useRef } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import { ModuleRegistry, ValueSetterParams, ISelectCellEditorParams } from "@ag-grid-community/core";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
+import { RichSelectModule } from "@ag-grid-enterprise/rich-select";
+ModuleRegistry.registerModules([ClientSideRowModelModule, RichSelectModule]);
+
+const Table = (data: any) => {
+  const columnsConfig = data.columnData;
+  const rowConfig = data.rowData;
+
+  console.log("column Data", columnsConfig);
+  console.log("row Data", rowConfig);
+
+  const gridApi = useRef<AgGridReact>(null);
+
+  // value setter function
+  function numberValueSetter(params: ValueSetterParams) {
+    let selectedRow = params.colDef.field;
+    Object.keys(rowConfig).forEach((k: any) => {
+      // @ts-ignore
+      rowConfig[k].forEach((obj: any) => {
+        // if (selectedField === undefined) {
+        //   return true;
+        // }
+        //@ts-ignore
+        if (obj[selectedRow] === params.newValue) {
+          for (let d in obj) {
+            params.data[k][d] = obj[d];
+          }
+        }
+      });
+    });
+    return true;
+  }
+
+  // Row Value mapping
+  function rowValue(columnName: any, fields: any) {
+    let listValues: any = [];
+    // @ts-ignore
+    rowConfig[fields].forEach((data) => {
+      // @ts-ignore
+      listValues.push(data[columnName]);
+    });
+    return listValues;
+  }
+
+  // column field convertions
+  let columnDefs: any = [];
+  columnsConfig.forEach((data: any) => {
+    let sub_fields = { [data["field"]]: data["sub_fields"] };
+    let children: any = [];
+    sub_fields[data["field"]].forEach((items: any) => {
+      children.push({
+        field: items,
+
+        cellEditor: "agSelectCellEditor",
+        cellEditorParams: {
+          values: rowValue(items, data["field"]),
+        } as ISelectCellEditorParams,
+        valueSetter: numberValueSetter,
+        valueGetter: (params: any) => {
+          return params.data[data["field"]][items];
+        },
+      });
+    });
+    columnDefs.push({
+      headerName: data["field"],
+      children: children,
+    });
+  });
+
+  const defaultColDef = useMemo(
+    () => ({
+      resizable: true,
+      flex: 1,
+      editable: true,
+      sortable: true,
+      animateRows: true,
+      floatingFilter: true,
+      filter: true,
+    }),
+    []
+  );
+
+  // add new data
+
+  function addItems(data: any) {
+    let addRowData: any = {};
+    Object.keys(data).forEach((item) => {
+      addRowData[item] = {};
+    });
+
+    gridApi.current?.api.applyTransaction({
+      add: [addRowData],
+    });
+  }
+
+  // const onGridReady = (params: any) => {
+  //   setApis(params.api);
+  //   fetch(rowConfigURL)
+  //     .then((resp) => resp.json())
+  //     .then((resp) => setrowConfig(resp));
+  // };
+
+  return (
+    <div className="ag-theme-alpine" style={{ height: 720, width: "100%" }}>
+      <button onClick={() => addItems(rowConfig)}>Add Items</button>
+      <AgGridReact
+        ref={gridApi}
+        rowData={[rowConfig]}
+        columnDefs={columnDefs}
+        defaultColDef={defaultColDef}
+        // onGridReady={onGridReady}
+      ></AgGridReact>
+    </div>
+  );
+};
+
+export default Table;
